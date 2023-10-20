78273e8efbb2062be73ac6a983bcd1630f98be43
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-10-19 20:54:55 -0500

    changed location of heroku script

--- client/src-sw.js
+++ client/src-sw.js
@@ -35,6 +35,10 @@ registerRoute(
        // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200]
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60
       })
     ]
   })
